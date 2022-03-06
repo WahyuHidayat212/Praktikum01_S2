eb33dcb22797757b46f5118caf155b8176714202
WahyuHidayat212 <93899685+WahyuHidayat212@users.noreply.github.com>
2022-03-06 22:00:26 +0700

    Add files via upload

---
+++ variable_user.php
@@ -0,0 +1,13 @@
+<?php
+    // definisikan variabel
+    $nama = 'Wahyu Hidayat';
+    $umur = 19;
+    $berat = 22.4;
+
+    echo 'Nama : ' . $nama;
+    echo '<br/>umur : ' . $umur. ' Tahun';
+    echo '<br/>berat : '.$berat.' Kg';
+
+    echo "<br/>Hello $nama Apakabar";
+
+    ?>
